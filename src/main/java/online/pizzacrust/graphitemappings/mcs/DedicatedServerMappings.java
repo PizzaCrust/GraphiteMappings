745b9c46ba5add0ed4116a007954733e062f6d2b
PizzaCrust <pizzacrustcoder@gmail.com>
2017-02-18 19:29:59 +0700

    ServerEula and PropertyManager

--- src/main/java/online/pizzacrust/graphitemappings/mcs/DedicatedServerMappings.java
+++ src/main/java/online/pizzacrust/graphitemappings/mcs/DedicatedServerMappings.java
@@ -1,13 +1,19 @@
 package online.pizzacrust.graphitemappings.mcs;
 
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TypeInsnNode;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.jar.JarFile;
 
+import jdk.internal.org.objectweb.asm.Opcodes;
 import online.pizzacrust.graphitemappings.MappingsBase;
 import online.pizzacrust.graphitemappings.TypeMappings;
 
@@ -48,7 +54,17 @@ public class DedicatedServerMappings extends MappingsBase {
                 });
             }
             for (MethodNode methodNode : classNode.methods) {
+                AtomicBoolean captureNextNew = new AtomicBoolean(false);
                 methodNode.instructions.iterator().forEachRemaining((insnNode) -> {
+                    if (insnNode instanceof TypeInsnNode) {
+                        TypeInsnNode typeInsnNode = (TypeInsnNode) insnNode;
+                        if (typeInsnNode.getOpcode() == Opcodes.NEW && captureNextNew.get()) {
+                            getMappings().putClass(Type.getType(typeInsnNode.desc)
+                                    .getInternalName(), "net.minecraft.server.dedicated" +
+                                    ".PropertyManager");
+                            captureNextNew.set(false);
+                        }
+                    }
                     if (insnNode instanceof LdcInsnNode) {
                         LdcInsnNode ldcInsnNode = (LdcInsnNode) insnNode;
                         if (ldcInsnNode.cst instanceof String) {
@@ -56,6 +72,21 @@ public class DedicatedServerMappings extends MappingsBase {
                             if (cst.equals("Server console handler")) {
                                 getMappings().putMethod(createObfMd(methodNode), createRemappedMd
                                         ("init", methodNode));
+                            }
+                            if (cst.equals("Loading properties")) {
+                                captureNextNew.set(true);
+                            }
+                            if (cst.equals("eula.txt")) {
+                                // next 2 instructions
+                                AbstractInsnNode abstractInsnNode = insnNode.getNext().getNext();
+                                if (abstractInsnNode instanceof MethodInsnNode) {
+                                    MethodInsnNode methodInsnNode = (MethodInsnNode)
+                                            abstractInsnNode;
+                                    if (methodInsnNode.getOpcode() == Opcodes.INVOKESPECIAL) {
+                                        getMappings().putClass(methodInsnNode.owner, "net" +
+                                                ".minecraft.server.ServerEula");
+                                    }
+                                }
                             }
                         }
                     }
